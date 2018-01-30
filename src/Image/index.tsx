a824d6af8b3362648a25d3acbe471cd1a98e646f
Maciej Chałapuk <m.chalapuk@quedex.net>
2018-01-30 20:43:29 +0100

    + bouding rect based image loading

--- src/Image/index.tsx
+++ src/Image/index.tsx
@@ -16,12 +16,16 @@ export interface State {
 }
 
 export class Image extends React.Component<Props, State> {
+  private container : HTMLDivElement;
+
   constructor(props : Props) {
     super(props);
 
     this.state = {
       step : 'static',
     };
+
+    this.maybeStartLoading = this.maybeStartLoading.bind(this);
   }
 
   render() {
@@ -34,6 +38,7 @@ export class Image extends React.Component<Props, State> {
       <div
         className={ `image ${s.container} ${s[step]}` }
         style={ { height: `${height}vw` } }
+        ref={ e => this.container = e as HTMLDivElement }
       >
         <img
           className={ s.image }
@@ -55,13 +60,38 @@ export class Image extends React.Component<Props, State> {
   }
 
   componentDidMount() {
-    this.setStep('loading');
+    window.addEventListener('scroll', this.maybeStartLoading);
+    window.addEventListener('resize', this.maybeStartLoading);
+    this.maybeStartLoading();
+  }
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.maybeStartLoading);
+    window.removeEventListener('resize', this.maybeStartLoading);
   }
 
+  private maybeStartLoading() {
+    if (this.state.step !== 'static' || !isInViewport(this.container, 128)) {
+      return;
+    }
+    this.setStep('loading');
+  }
   private setStep(step : LoadingStep) {
     this.setState(prev => ({ ...prev, step }));
   }
 }
 
 export default Image;
+
+function isInViewport(element : HTMLElement, margin : number = 0) {
+  const rect = element.getBoundingClientRect();
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight
+
+  const upperTrigger = 0 + margin;
+  const lowerTrigger = windowHeight - margin;
+
+  return !(
+    (rect.top <= upperTrigger && rect.bottom <= upperTrigger)
+    || (rect.top >= lowerTrigger && rect.bottom >= lowerTrigger)
+  );
+}
 
