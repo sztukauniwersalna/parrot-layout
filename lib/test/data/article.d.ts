8542da47cf2ceef3d3ff22b59c6c050da86d7c8b
Maciej Chałapuk <maciej@chalapuk.pl>
2019-04-09 00:03:51 +0200

    ~ loader uses tsconfig for test project

--- lib/test/data/article.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-export declare function ArticlePage(): JSX.Element;
-export default ArticlePage;
--- test.config.js
+++ test.config.js
@@ -51,7 +51,10 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        loader: 'ts-loader',
+        options: {
+          configFile: path.resolve(__dirname, 'test/tsconfig.json'),
+        },
       },
       {
         enforce: 'pre',
